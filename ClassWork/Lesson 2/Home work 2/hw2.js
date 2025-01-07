813421c8f020cd181ed870f69e9ef51135acb1bb
Ruslan07r <ruslan_m@ukr.net>
2025-01-07 22:25:00 +0200

    HW2

---
+++ ClassWork/Lesson 2/Home work 2/hw2.js
@@ -0,0 +1,6 @@
+document.querySelector("#btn").onclick = function(){
+        // alert(new Date())
+        let = output = document.querySelector("#output");
+        output.innerHTML= "Ruslan";
+        output.style.color = "red";
+}
